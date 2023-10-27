b5ab4c921c040df206b885cf311972692fd6675d
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-10-27 18:22:37 -0300

    chore: cria app

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const swaggerUi = require('swagger-ui-express')
-const specs = require('./swagger')
+// const swaggerUi = require('swagger-ui-express')
+// const specs = require('./swagger')
 const routes = require('./routes')
 
 
 const port = 3000;
 
 const app = express()
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 routes(app)
 
 app.listen(3000, () => {
